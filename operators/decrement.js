db7e2a1d07b3256a926fe5ce5186882723ec7913
Mounica500d <99234448+Mounica500d@users.noreply.github.com>
2022-02-13 13:44:27 +0530

    Create decrement.js

---
+++ operators/decrement.js
@@ -0,0 +1,16 @@
+//decrement
+
+let cooler = 4000;
+
+console.log(cooler--);
+console.log(cooler);
+
+console.log(--cooler);
+
+
+//decrement 2
+
+let tv = 10000;
+
+console.log('tv', --tv);
+console.log('tv', tv--);
